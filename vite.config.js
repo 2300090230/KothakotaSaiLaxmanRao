d847e610a7e40c450187e85f06c5982a8f5174ca
Laxman <2300090230@kluniversity.in>
2025-08-09 00:56:58 +0530

    Fix SPA routing on Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+
+export default {
   plugins: [react()],
-   base: '/',
-})
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist'
+  },
+  base: '/'
+}
+
